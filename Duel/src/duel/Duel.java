6841ca3d38bbf54de3aa3583eae489b2fc74eaed
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-09-13 15:19:37 -0400

    commit

--- Duel/src/duel/Duel.java
+++ Duel/src/duel/Duel.java
@@ -1,5 +1,32 @@
 package duel;
 
+
+
+
 public class Duel {
 
+	
+
+	private Dueler d1;
+	private Dueler d2;
+	
+	
+	
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
+	}
+	
+	public Duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		
+	}
+	
+
 }
+
