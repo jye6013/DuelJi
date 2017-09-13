bff6333c5a7b5fffe7ad4fb2efbba58c4abd19d2
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-13 15:22:48 -0400

    dueler

---
+++ Duel/src/duel/Dueler.java
@@ -0,0 +1,12 @@
+package duel;
+
+public interface Dueler {
+	
+	//print a taunt
+		//later: the taunt must be different each time the method is called
+		void taunt();
+		
+		//returns the name of the Dueler
+		String getName();
+		
+}
