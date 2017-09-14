17f02dd038008a51680fca137c36c70f446dcdd6
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-09-14 15:19:12 -0400

    commit

--- Duel/src/duel/Duel.java
+++ Duel/src/duel/Duel.java
@@ -1,11 +1,21 @@
 package duel;
 
+
+
+
 public class Duel {
+
+	
 
 	private Dueler d1;
 	private Dueler d2;
+	private int dueler1HP = 30;
+	private int dueler2HP = 30;
 	
-		public static void main(String[] args){
+	
+	
+	
+	public static void main(String[] args){
 		Duel duel = new Duel();
 		duel.dukeItOut();
 	}
@@ -16,7 +26,40 @@ public class Duel {
 	}
 	
 	public void dukeItOut(){
+		System.out.print(d1.getName());
+		pause(600);
+		System.out.print(" vs ");
+		pause(600);
+		System.out.println(d2.getName());
+		pause(600);
+		d1.taunt();
+		pause(2000);
+		d2.taunt();
+		pause(2000);
+		d1.setStartingHP(dueler1HP);
+		d2.setStartingHP(dueler2HP);
+		
+		boolean d1SaysD2Fair = d1.determineIfOpponentIsFair(d2, 100);
+		boolean d2SaysD1Fair = d2.determineIfOpponentIsFair(d1, 100);
+		
+
 		
 	}
+	
+	
+	/**
+	 * This method involves "Threads" which we will get to much later in the year, essentially, it
+	 * creates a pause in the execution of the program, which makes it more dramatic
+	 * @param time
+	 */
+	public void pause(long time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
 }
 
--- Duel/src/duel/Dueler.java
+++ Duel/src/duel/Dueler.java
@@ -1,11 +1,21 @@
 package duel;
 
 public interface Dueler {
+
 	
 	//print a taunt
-		//later: the taunt must be different each time the method is called
-		void taunt();
-		
-		//returns the name of the Dueler
-		String getName();
+	//later: the taunt must be different each time the method is called
+	void taunt();
+	
+	//returns the name of the Dueler
+	String getName();
+	
+	//set the value of a field. Challenge: Write this in such a way that it can only be called once (by the Duel, at the start)
+	void setStartingHP(int hp);
+	//returns the value of HP, can be called as often as anyone wants
+	int getHP();
+	
+	//This method is called by the Duel and can be used by the dueler
+	//return true if d has its HP equal to "target"
+	boolean determineIfOpponentIsFair(Dueler d, int target);
 }
