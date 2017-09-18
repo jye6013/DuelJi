1e6710e636b4827efff4045b794016f24b1de0d2
jessi <jessi@DESKTOP-8072EKE.home>
2017-09-17 23:08:00 -0400

    taunt

--- Duel/src/duel/CharacterA.java
+++ Duel/src/duel/CharacterA.java
@@ -3,20 +3,26 @@ package duel;
 public class CharacterA implements Duel {
 	
 	private String name;
-	
+	private String taunt;
 	
 	public CharacterA() {
-		public String getName(){
-			name = "Ji";
-			return name;
-		}
+		
 		
 	}
 	
 	public void taunt() {
-		
+		taunt = "Can't defeat me!";
+		return taunt; 
 	}
 
-
+	public String getName(){
+		name = "Ji";
+		return name;
+	}
+	
+	public int d1LastAction() {
+		
+	}
+	
 }
 
--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -3,6 +3,7 @@ package duel;
 public class CharacterB implements Duel{
 	
 	private String name;
+	private String taunt;
 	
 	public CharacterB() {
 		
@@ -10,11 +11,16 @@ public class CharacterB implements Duel{
 	}
 	
 	public void taunt() {
-		
+		taunt = "How weak!";
+		return taunt; 
 	}
 
 	public String getName(){
 		name = "Jessica";
 		return name;
+	}
+	
+	public int d2LastAction() {
+		
 	}
 }
