00c0637307bf58e9b1e11559dae0ba338b0984ae
jessi <jessi@DESKTOP-8072EKE.home>
2017-09-18 20:59:08 -0400

    finished last three

--- Duel/src/duel/CharacterA.java
+++ Duel/src/duel/CharacterA.java
@@ -4,6 +4,9 @@ public class CharacterA implements Dueler {
 	
 	private String name;
 	private String taunt;
+	private boolean loadedGun;
+	private String tauntArray[] = {"Can't defeat me.", "Try again!", "You're too weak.", 
+	"Why don't you try something else?"};
 	
 	public CharacterA() {
 
@@ -11,9 +14,11 @@ public class CharacterA implements Dueler {
 	
 	}
 	
+	static int randomNum = Math.random()*4;
+	
 	public void taunt() {
-		taunt = "Can't defeat me!";
-		return taunt; 
+		
+		return tauntArray[randomNum];
 	}
 
 	
@@ -23,24 +28,47 @@ public class CharacterA implements Dueler {
 	}
 
 	public boolean determineIfOpponentIsFair (Dueler d, int hp) {
-		if (d == d2 && d2.getHp <= hp)
+		if (d.getHp == hp)
 		{
 			return true; 
 		}
 	}
 	
 	
-	public int getAction(Object caller) {
-		if (caller != Duel.d2)
+	public int getAction(Object caller) 
+	{
+		if (caller instanceof Duel) //duel is calling it
 		{
-			return int (Math.random()*4);
+			if(loadedGun == true && Math.random() < .5) //is gun loaded? and 50% chance
+			{
+				loadedGun = false; //shoot the gun then it unloads
+				return Duel.SHOOTING;
+			}
+			else
+			{
+				if(!loadedGun && Math.random() < .4) //gun not loaded and 40% chance
+				{
+					loadedGun = true;//loaded gun
+					return Duel.LOADING;
+					
+				}
+				else
+				{
+					return Duel.GUARDING; //no other option but to guard
+				}
+			}
+		}
+		else
+		{
+			return Duel.YEAH_RIGHT;
+			return int (Math.random()*3); //random move
 		}
 	}
 	
 	public int hit(Object caller) {
 		if(caller instanceof Duel)
 		{
-			//newHp = whatever the variable for hp - 10; has to go to getHp();
+			this.hp = this.hp - 10;
 		}
 	}
 	
--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -4,15 +4,19 @@ public class CharacterB implements Dueler{
 	
 	private String name;
 	private String taunt;
+	private boolean loadedGun;
+	private String tauntArray[] = {"Too weak for me.", "Similar to a child!", "Can't defeat me!", 
+	"Try something else since that won't work"};
 	
 	public CharacterB() {
 	
 		
 	}
 	
+	static int randomNum = Math.random()*4;
+		
 	public void taunt() {
-		taunt = "How weak!";
-		return taunt; 
+		System.out.println(tauntArray[randomNum]);
 	}
 
 	public String getName(){
@@ -21,23 +25,45 @@ public class CharacterB implements Dueler{
 	}
 	
 	public boolean determineIfOpponentIsFair (Dueler d, int hp) {
-		if (d == d1 && d1.getHp <= hp)
+		if (d.getHp == hp)
 		{
 			return true; 
 		}
 	}
 	
 	public int getAction(Object caller) {
-		if (caller != Duel.d1)
+		if (caller instanceof Duel) //duel is calling it
 		{
-			return int (Math.random()*4);
+			if(loadedGun == true && Math.random() < .5) //is gun loaded? and 50% chance
+			{
+				loadedGun = false; //shoot the gun then it unloads
+				return Duel.SHOOTING;
+			}
+			else
+			{
+				if(!loadedGun && Math.random() < .4) //gun not loaded and 40% chance
+				{
+					loadedGun = true;//loaded gun
+					return Duel.LOADING;
+					
+				}
+				else
+				{
+					return Duel.GUARDING; //no other option but to guard
+				}
+			}
+		}
+		else
+		{
+			return Duel.YEAH_RIGHT;
+			return int (Math.random()*3); //random move
 		}
 		
 	}
 	public int hit(Object caller) {
 		if(caller instanceof Duel)
 		{
-			//newHp = whatever the variable for hp - 10; has to go to getHp();
+			this.hp = this.hp - 10;
 		}
 	}
 }
